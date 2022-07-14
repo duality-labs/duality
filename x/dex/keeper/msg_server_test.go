cb922da989476ba95cd2fb424dc65866a7791a1e
colinfink <colinfinkbeiner1@gmail.com>
2022-07-14 17:27:10 +0200

    dex init

---
+++ x/dex/keeper/msg_server_test.go
@@ -0,0 +1,16 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "github.com/NicholasDotSol/duality/testutil/keeper"
+	"github.com/NicholasDotSol/duality/x/dex/keeper"
+	"github.com/NicholasDotSol/duality/x/dex/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.DexKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
