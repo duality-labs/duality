09a788b96acac7ed586e4da63afce08822308285
finkbeca <colinfinkbeiner1@gmail.com>
2022-08-10 11:24:31 -0700

    added price to sort

--- x/dex/keeper/sort_tokens.go
+++ x/dex/keeper/sort_tokens.go
@@ -1,57 +1,57 @@
 package keeper
 
 import (
-	"crypto/sha256"
 	"bytes"
+	"crypto/sha256"
+
 	//"strings"
 	"github.com/NicholasDotSol/duality/x/dex/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func (k Keeper) SortTokens(ctx sdk.Context, token0 string, token1 string) (string, string, error) {
+func (k Keeper) SortTokens(ctx sdk.Context, token0 string, token1 string, price sdk.Dec) (string, string, sdk.Dec, error) {
 
 	// Calculate sha256 Checksum for Token0, Token1
 	token0Hash := sha256.Sum256([]byte(token0))
 	token1Hash := sha256.Sum256([]byte(token1))
 
 	//calculates an comparison integer for the two hashes
-	comparisonInt :=  bytes.Compare(token0Hash[:], token1Hash[:])
+	comparisonInt := bytes.Compare(token0Hash[:], token1Hash[:])
 
 	/* If comparisonInt == -1 : token0Hash < token1Hash
 	   comparisonInt == 0 token0Hash == token1Hash (return an error)
 	   comparisonInt == 1 token0Hash > token1Hash (switch elements)
 	*/
 	if comparisonInt == -1 {
-		return token0, token1, nil
+		return token0, token1, price, nil
 	} else if comparisonInt == 0 {
-		return "", "",  sdkerrors.Wrapf(types.ErrInvalidTokenPair, "Not a valid Token Pair: tokenA and tokenB cannot be the same")
+		return "", "", sdk.ZeroDec(), sdkerrors.Wrapf(types.ErrInvalidTokenPair, "Not a valid Token Pair: tokenA and tokenB cannot be the same")
 	} else {
-		return token1, token0, nil
+		return token1, token0, sdk.OneDec().Quo(price), nil
 	}
-	
 
 }
 
-func (k Keeper) SortTokensDeposit(ctx sdk.Context, token0 string, token1 string, amounts0 []sdk.Dec, amounts1 []sdk.Dec) (string, string, []sdk.Dec, []sdk.Dec, error) {
+func (k Keeper) SortTokensDeposit(ctx sdk.Context, token0 string, token1 string, amounts0 []sdk.Dec, amounts1 []sdk.Dec, price sdk.Dec) (string, string, []sdk.Dec, []sdk.Dec, sdk.Dec, error) {
 
 	// Calculate sha256 Checksum for Token0, Token1
 	token0Hash := sha256.Sum256([]byte(token0))
 	token1Hash := sha256.Sum256([]byte(token1))
 
 	//calculates an comparison integer for the two hashes
-	comparisonInt :=  bytes.Compare(token0Hash[:], token1Hash[:])
+	comparisonInt := bytes.Compare(token0Hash[:], token1Hash[:])
 
 	/* If comparisonInt == -1 : token0Hash < token1Hash (returns parameters as given)
 	   comparisonInt == 0 token0Hash == token1Hash (return an error)
 	   comparisonInt == 1 token0Hash > token1Hash (switch elements and amount arrays)
-	*/ 
+	*/
 	if comparisonInt == -1 {
-		return token0, token1, amounts0, amounts1, nil
+		return token0, token1, amounts0, amounts1, price, nil
 	} else if comparisonInt == 0 {
-		return "", "", nil, nil,  sdkerrors.Wrapf(types.ErrInvalidTokenPair, "Not a valid Token Pair: tokenA and tokenB cannot be the same")
+		return "", "", nil, nil, sdk.ZeroDec(), sdkerrors.Wrapf(types.ErrInvalidTokenPair, "Not a valid Token Pair: tokenA and tokenB cannot be the same")
 	} else {
-		return token1, token0, amounts1, amounts0, nil
+		return token1, token0, amounts1, amounts0, sdk.OneDec().Quo(price), nil
 	}
 
 }
