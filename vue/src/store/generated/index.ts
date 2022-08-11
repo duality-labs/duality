60a77be382ad776aad9d2b3605ceb1dacc020b05
finkbeca <colinfinkbeiner1@gmail.com>
2022-08-11 11:30:46 -0700

    tickspacing init

--- vue/src/store/generated/index.ts
+++ vue/src/store/generated/index.ts
@@ -1,5 +1,6 @@
 // THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
 
+import NicholasDotSolDualityNicholasdotsolDualityDex from './NicholasDotSol/duality/nicholasdotsol.duality.dex'
 import CosmosCosmosSdkCosmosAuthV1Beta1 from './cosmos/cosmos-sdk/cosmos.auth.v1beta1'
 import CosmosCosmosSdkCosmosBankV1Beta1 from './cosmos/cosmos-sdk/cosmos.bank.v1beta1'
 import CosmosCosmosSdkCosmosBaseTendermintV1Beta1 from './cosmos/cosmos-sdk/cosmos.base.tendermint.v1beta1'
@@ -23,6 +24,7 @@ import CosmosIbcGoIbcCorePortV1 from './cosmos/ibc-go/ibc.core.port.v1'
 
 
 export default { 
+  NicholasDotSolDualityNicholasdotsolDualityDex: load(NicholasDotSolDualityNicholasdotsolDualityDex, 'nicholasdotsol.duality.dex'),
   CosmosCosmosSdkCosmosAuthV1Beta1: load(CosmosCosmosSdkCosmosAuthV1Beta1, 'cosmos.auth.v1beta1'),
   CosmosCosmosSdkCosmosBankV1Beta1: load(CosmosCosmosSdkCosmosBankV1Beta1, 'cosmos.bank.v1beta1'),
   CosmosCosmosSdkCosmosBaseTendermintV1Beta1: load(CosmosCosmosSdkCosmosBaseTendermintV1Beta1, 'cosmos.base.tendermint.v1beta1'),
