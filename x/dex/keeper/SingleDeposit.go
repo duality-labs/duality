02e627e763d9897bc84ae0627289959b2c9408b9
ratankaliani <ratankaliani@berkeley.edu>
2022-08-16 13:29:06 -0700

    Update sudo logic for withdraw, swap

--- x/dex/keeper/SingleDeposit.go
+++ x/dex/keeper/SingleDeposit.go
@@ -38,22 +38,16 @@ func (k msgServer) SingleDeposit(goCtx context.Context, token0 string, token1 st
 func (k msgServer) SingleWithdraw(goCtx context.Context, token0 string, token1 string, amount sdk.Dec, msg *types.MsgAddLiquidity, callerAdr sdk.AccAddress, receiver sdk.AccAddress) error {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	/*
-			// Check if the pair already exists
 			1) Find Pair
 			   a) If pair exists, get the pair
-			   b) If pair does not exist, init pair
-			       i) If nodes do not exist, init nodes
-				       1) Add outgoingEdges to storage
-				   ii) If nodes exist
-					   1) Add tokenA, tokenB to eachother's outgoingEdges
+			   b) If pair does not exist, exit
+
 			2) Find Tick
-		       a) If virtual price tick/index does not exist, add new index
-				    i) Initialize index to 1 in bitmap
-					ii) Create new tick w/ amount in virtual_price_tick_list
-					iii) Add tick w/ amount to virtual_price_tick_queue
+		       a) If virtual price tick/index does not exist, exit
 			   b) If exists
-					i) Update tick (+= amount) in virtual_price_tick_list
-					ii) Add amount to existing tick in corresponding queue
+					i) Update tick (-= amount) in virtual_price_tick_list
+					ii) Subtract amount from existing tick in corresponding queue
+					iii) If tick cleared, uninitialize index in bitmap, remove from virtual_price_tick_list & remove from queue
 			3) Update Shares
 				i) TBD
 	*/
@@ -62,27 +56,21 @@ func (k msgServer) SingleWithdraw(goCtx context.Context, token0 string, token1 s
 	return nil
 }
 
+// Need to figure out logic for route vs. swap
 func (k msgServer) SingleSwap(goCtx context.Context, token0 string, token1 string, amount sdk.Dec, msg *types.MsgAddLiquidity, callerAdr sdk.AccAddress, receiver sdk.AccAddress) error {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	/*
-			// Check if the pair already exists
-			1) Find Pair
-			   a) If pair exists, get the pair
-			   b) If pair does not exist, init pair
-			       i) If nodes do not exist, init nodes
-				       1) Add outgoingEdges to storage
-				   ii) If nodes exist
-					   1) Add tokenA, tokenB to eachother's outgoingEdges
-			2) Find Tick
-		       a) If virtual price tick/index does not exist, add new index
-				    i) Initialize index to 1 in bitmap
-					ii) Create new tick w/ amount in virtual_price_tick_list
-					iii) Add tick w/ amount to virtual_price_tick_queue
-			   b) If exists
-					i) Update tick (+= amount) in virtual_price_tick_list
-					ii) Add amount to existing tick in corresponding queue
-			3) Update Shares
-				i) TBD
+		1) Find Pair
+		   a) If pair exists, get the pair
+		   b) If pair does not exist, exit
+		2) Get CurrTick & corresponding list for direction
+		3) Attempt to swap amount through the ticks in pair
+			i) Loop through queue for virtual tick & empty ticks
+			ii) If queue empty, query next virtualTick from bitmap
+			iii) Continue looping until amount == 0
+		4) Perform swap
+		5) Update Shares
+			i) TBD
 	*/
 
 	_ = ctx
