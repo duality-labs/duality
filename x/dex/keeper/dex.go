3f2d7d6f418f9c335d4e86ef2f639e20ebeab28f
ratankaliani <ratankaliani@berkeley.edu>
2022-08-16 15:08:23 -0700

    pushing pseudo-logic

--- x/dex/keeper/dex.go
+++ x/dex/keeper/dex.go
@@ -8,6 +8,33 @@ import (
 	//sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+func (k msgServer) CreateNewPair(goCtx context.Context, token0 string, token1 string, amount sdk.Dec, msg *types.MsgAddLiquidity, callerAdr sdk.AccAddress, receiver sdk.AccAddress) error {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	/*
+			// Check if the pair already exists
+			1) Find Pair
+			   a) If pair exists, get the pair
+			   b) If pair does not exist, init pair
+			       i) If nodes do not exist, init nodes
+				       1) Add outgoingEdges to storage
+				   ii) If nodes exist
+					   1) Add tokenA, tokenB to eachother's outgoingEdges
+			2) Find Tick
+		       a) If virtual price tick/index does not exist, add new index
+				    i) Initialize index to 1 in bitmap
+					ii) Create new tick w/ amount in virtual_price_tick_list
+					iii) Add tick w/ amount to virtual_price_tick_queue
+			   b) If exists
+					i) Update tick (+= amount) in virtual_price_tick_list
+					ii) Add amount to existing tick in corresponding queue
+			3) Update Shares
+				i) TBD
+	*/
+
+	_ = ctx
+	return nil
+}
+
 func (k msgServer) SingleDeposit(goCtx context.Context, token0 string, token1 string, amount sdk.Dec, msg *types.MsgAddLiquidity, callerAdr sdk.AccAddress, receiver sdk.AccAddress) error {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	/*
@@ -68,8 +95,10 @@ func (k msgServer) SingleSwap(goCtx context.Context, token0 string, token1 strin
 			i) Loop through queue for virtual tick & empty ticks
 			ii) If queue empty, query next virtualTick from bitmap
 			iii) Continue looping until amount == 0
+			iv) Store last tick, will be new currTick
 		4) Perform swap
-		5) Update Shares
+		5) Update CurrTick
+		6) Update Shares
 			i) TBD
 	*/
 
