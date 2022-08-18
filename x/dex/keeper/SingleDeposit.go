955ac43425f1740f72adca3a181d735519388779
ratankaliani <ratankaliani@berkeley.edu>
2022-08-17 18:21:30 -0700

    Add method to calculate tick for virtualPrice

--- x/dex/keeper/SingleDeposit.go
+++ x/dex/keeper/SingleDeposit.go
@@ -3,21 +3,34 @@ package keeper
 import (
 	"context"
 
+	"math"
+
 	"github.com/NicholasDotSol/duality/x/dex/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func (k Keeper) CalculateVirtualPrice(token0 string, token1 string, tokenDirection string, amout sdk.Dec, fee sdk.Dec, price sdk.Dec) (sdk.Dec, error) {
+func (k Keeper) CalculateVirtualPrice(token0 string, token1 string, tokenDirection string, amount sdk.Dec, fee sdk.Dec, price sdk.Dec) (sdk.Dec, error) {
 
 	if token0 == tokenDirection {
 
-		return fee.Quo(price.Mul(sdk.NewDec(100000))), nil
+		return fee.Quo(price.Mul(sdk.NewDec(10000))), nil
 	} else if token1 == tokenDirection {
 		// pools[j].Price.Mul(pools[j].Fee)).Quo(sdk.NewDec(10000)))
 		return price.Mul(fee).Quo(sdk.NewDec(10000)), nil
 	}
 	return sdk.ZeroDec(), nil
+
+}
+
+// Returns corresponding tick index for a given virtualPrice
+func (k Keeper) CalculateTick(virtualPrice sdk.Dec) (sdk.Dec, error) {
+
+	// Ticks are 1bp apart
+	fVirtualPrice, _ := virtualPrice.Float64()
+	index := math.Log(fVirtualPrice) / math.Log(1.0001)
+	// Always takes floor of index
+	return sdk.NewDec(int64(index)), nil
 
 }
 
