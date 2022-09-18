ce8a2eabeb79a589fe9ab1b71e89072d78a1c5c5
David Uhlmann <dib542@gmail.com>
2022-09-18 21:33:50 +1000

    Add startup script check to ensure seed RPC endpoint is reachable

--- testnet/scripts/startup.sh
+++ testnet/scripts/startup.sh
@@ -18,5 +18,17 @@ else
 
     echo "Have seed RPC: $RPC_ADDRESS"
     echo "Will attempt to join the network"
+    echo "Contacting network..."
+
+    # check if we can get information from the current network
+    abci_info_json=$( wget --tries 30 -q -O - $RPC_ADDRESS/abci_info )
+    if [[ "$( echo $abci_info_json | jq -r ".result.response.data" )" == "duality" ]]
+    then
+        echo "Found Duality chain!"
+    else
+        echo "Could not establish connection to Duality chain"
+        echo "Exiting..."
+        exit 1
+    fi
 
 fi
