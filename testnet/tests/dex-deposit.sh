482aec3731cea53ad61607ccffb7acaa989cef1e
David Uhlmann <dib542@gmail.com>
2022-11-02 00:53:12 +1000

    Fix test to work as intended

--- testnet/tests/dex-deposit.sh
+++ testnet/tests/dex-deposit.sh
@@ -6,15 +6,23 @@ test_name="dex: can make deposit"
 # todo: check state before? (may require test in serious)
 # bash /root/.duality/scripts/expect_state_value_length_to_be.sh ".app_state.dex.sharesList" 0
 
+echo "$test_name: setup"
+person=$(openssl rand -hex 12)
+dualityd keys add $person <<< $'asdfasdf\nn'
+dualityd tx bank send $(dualityd keys show fred --output json | jq -r .address) $(dualityd keys show $person --output json | jq -r .address) 1000tokenA,1000tokenB -y --broadcast-mode block --output json
+
+echo "$test_name: start test"
 # deposit tokens
 # wait for tx to be processed and return an exit code
-tx_result=$(dualityd tx dex deposit --node tcp://dualitynode0:26657 $(dualityd keys show "node$ID" --output json | jq -r .address) token stake 0.0000000001 0.0000000001 1 0 --from "node$ID" --yes --output json --broadcast-mode block)
+tx_result=$(dualityd tx dex deposit $(dualityd keys show $person --output json | jq -r .address) tokenA tokenB 0.0000000000000001 0.0000000000000001 1 0 --from "$person" --yes --output json --broadcast-mode block)
 tx_code=$(echo $tx_result | jq -r .code)
 if [[ "$tx_code" != "0" ]]
 then
-    echo "Error at $test_name: $tx_result"
+    echo "$test_name error $tx_code at $test_name"
     exit $tx_code
 fi
+
+echo "$test_name: Deposited coins to ticks"
 
 # todo: time the above function, it is a rough calculation of round trip processing time of the function
 
