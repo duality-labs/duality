e3ee1b92449cbedafa6612d988ea6515adc28386
Teddy Knox <teddy@stake.fish>
2022-11-11 00:42:23 -0500

    Setup unified MsgServerTestSuite

--- x/dex/keeper/msg_server_limit_test.go
+++ x/dex/keeper/msg_server_limit_test.go
@@ -1,24 +1,12 @@
 package keeper_test
 
 import (
-	"context"
-	"fmt"
-	"testing"
-
-	dualityapp "github.com/NicholasDotSol/duality/app"
-	"github.com/NicholasDotSol/duality/x/dex/keeper"
 	. "github.com/NicholasDotSol/duality/x/dex/keeper/internal/testutils"
 	"github.com/NicholasDotSol/duality/x/dex/types"
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"github.com/stretchr/testify/suite"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-func (s *MsgServerLimitTestSuite) TestSingle() {
+func (s *MsgServerTestSuite) TestSingle() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -29,7 +17,7 @@ func (s *MsgServerLimitTestSuite) TestSingle() {
 	s.assertDexBalances(0, 50)
 }
 
-func (s *MsgServerLimitTestSuite) TestMultiple() {
+func (s *MsgServerTestSuite) TestMultiple() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -61,7 +49,7 @@ func (s *MsgServerLimitTestSuite) TestMultiple() {
 	s.assertDexBalances(0, 200)
 }
 
-func (s *MsgServerLimitTestSuite) TestDifferentReceiverAndCreator() {
+func (s *MsgServerTestSuite) TestDifferentReceiverAndCreator() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -81,7 +69,7 @@ func (s *MsgServerLimitTestSuite) TestDifferentReceiverAndCreator() {
 	s.assertDexBalances(0, 100)
 }
 
-func (s *MsgServerLimitTestSuite) TestFailUnrecognizedToken() {
+func (s *MsgServerTestSuite) TestFailUnrecognizedToken() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -97,7 +85,7 @@ func (s *MsgServerLimitTestSuite) TestFailUnrecognizedToken() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestFailInsufficientBalance() {
+func (s *MsgServerTestSuite) TestFailInsufficientBalance() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -113,7 +101,7 @@ func (s *MsgServerLimitTestSuite) TestFailInsufficientBalance() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestMultiTickLimitOrder1to0WithWithdraw() {
+func (s *MsgServerTestSuite) TestMultiTickLimitOrder1to0WithWithdraw() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -135,7 +123,7 @@ func (s *MsgServerLimitTestSuite) TestMultiTickLimitOrder1to0WithWithdraw() {
 	s.assertBobBalancesDec(NewDec(60), sdk.MustNewDecFromStr("239.9985"))
 }
 
-func (s *MsgServerLimitTestSuite) TestMultiTickLimitOrder0to1WithWithdraw() {
+func (s *MsgServerTestSuite) TestMultiTickLimitOrder0to1WithWithdraw() {
 	s.fundAliceBalances(100000, 500)
 	s.fundBobBalances(100, 200)
 
@@ -161,7 +149,7 @@ func (s *MsgServerLimitTestSuite) TestMultiTickLimitOrder0to1WithWithdraw() {
 	s.assertAliceBalancesDec(NewDec(99950), sdk.MustNewDecFromStr("534.000540032401944116"))
 }
 
-func (s *MsgServerLimitTestSuite) TestWithdrawFailsWhenNothingToWithdraw() {
+func (s *MsgServerTestSuite) TestWithdrawFailsWhenNothingToWithdraw() {
 	s.fundAliceBalances(100000, 500)
 	s.fundBobBalances(100, 200)
 
@@ -177,7 +165,7 @@ func (s *MsgServerLimitTestSuite) TestWithdrawFailsWhenNothingToWithdraw() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestFailsWhenWithdrawNotCalledByOwner() {
+func (s *MsgServerTestSuite) TestFailsWhenWithdrawNotCalledByOwner() {
 	s.fundAliceBalances(100000, 500)
 	s.fundBobBalances(100, 200)
 
@@ -195,7 +183,7 @@ func (s *MsgServerLimitTestSuite) TestFailsWhenWithdrawNotCalledByOwner() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestFailsWhenWrongKeyToken() {
+func (s *MsgServerTestSuite) TestFailsWhenWrongKeyToken() {
 	s.fundAliceBalances(100000, 500)
 	s.fundBobBalances(100, 200)
 
@@ -214,7 +202,7 @@ func (s *MsgServerLimitTestSuite) TestFailsWhenWrongKeyToken() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestFailsWhenWrongKey() {
+func (s *MsgServerTestSuite) TestFailsWhenWrongKey() {
 	s.fundAliceBalances(100000, 500)
 	s.fundBobBalances(100, 200)
 
@@ -233,7 +221,7 @@ func (s *MsgServerLimitTestSuite) TestFailsWhenWrongKey() {
 	s.Assert().Error(err)
 }
 
-func (s *MsgServerLimitTestSuite) TestCancelSingle() {
+func (s *MsgServerTestSuite) TestCancelSingle() {
 	s.fundAliceBalances(100, 500)
 
 	s.assertDexBalances(0, 0)
@@ -249,7 +237,7 @@ func (s *MsgServerLimitTestSuite) TestCancelSingle() {
 	s.assertDexBalances(0, 0)
 }
 
-func (s *MsgServerLimitTestSuite) TestCancelPartial() {
+func (s *MsgServerTestSuite) TestCancelPartial() {
 	s.fundAliceBalances(100, 500)
 
 	s.assertDexBalances(0, 0)
@@ -270,7 +258,7 @@ func (s *MsgServerLimitTestSuite) TestCancelPartial() {
 	s.assertDexBalances(0, 0)
 }
 
-func (s *MsgServerLimitTestSuite) TestProgressiveLimitOrderFill() {
+func (s *MsgServerTestSuite) TestProgressiveLimitOrderFill() {
 	s.fundAliceBalances(100, 500)
 	s.fundBobBalances(100, 200)
 
@@ -295,264 +283,4 @@ func (s *MsgServerLimitTestSuite) TestProgressiveLimitOrderFill() {
 	s.assertDexBalances(8, 50)
 
 	// TODO: How to verify current tick?
-}
-
-func TestMsgServerLimitTestSuite(t *testing.T) {
-	suite.Run(t, new(MsgServerLimitTestSuite))
-}
-
-type MsgServerLimitTestSuite struct {
-	suite.Suite
-
-	app         *dualityapp.App
-	msgServer   types.MsgServer
-	ctx         sdk.Context
-	queryClient types.QueryClient
-	alice       sdk.AccAddress
-	bob         sdk.AccAddress
-	goCtx       context.Context
-}
-
-func (s *MsgServerLimitTestSuite) SetupTest() {
-	app := dualityapp.Setup(false)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-
-	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
-	app.BankKeeper.SetParams(ctx, banktypes.DefaultParams())
-
-	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
-	types.RegisterQueryServer(queryHelper, app.DexKeeper)
-	queryClient := types.NewQueryClient(queryHelper)
-
-	accAlice := app.AccountKeeper.NewAccountWithAddress(ctx, s.alice)
-	app.AccountKeeper.SetAccount(ctx, accAlice)
-	accBob := app.AccountKeeper.NewAccountWithAddress(ctx, s.bob)
-	app.AccountKeeper.SetAccount(ctx, accBob)
-
-	// Set Fee List
-	app.DexKeeper.AppendFeeList(ctx, types.FeeList{0, 1})
-	app.DexKeeper.AppendFeeList(ctx, types.FeeList{1, 2})
-	app.DexKeeper.AppendFeeList(ctx, types.FeeList{2, 3})
-	app.DexKeeper.AppendFeeList(ctx, types.FeeList{3, 4})
-
-	s.app = app
-	s.msgServer = keeper.NewMsgServerImpl(app.DexKeeper)
-	s.ctx = ctx
-	s.goCtx = sdk.WrapSDKContext(ctx)
-	s.queryClient = queryClient
-	s.alice = sdk.AccAddress([]byte("alice"))
-	s.bob = sdk.AccAddress([]byte("bob"))
-}
-
-func (s *MsgServerLimitTestSuite) fundAccountBalancesDec(account sdk.AccAddress, aBalance sdk.Dec, bBalance sdk.Dec) {
-	aBalanceInt := sdk.NewIntFromBigInt(aBalance.BigInt())
-	bBalanceInt := sdk.NewIntFromBigInt(bBalance.BigInt())
-	balances := sdk.NewCoins(NewACoin(aBalanceInt), NewBCoin(bBalanceInt))
-	err := simapp.FundAccount(s.app.BankKeeper, s.ctx, account, balances)
-	s.Assert().NoError(err)
-	s.assertAccountBalancesDec(account, aBalance, bBalance)
-}
-
-func (s *MsgServerLimitTestSuite) fundAccountBalances(account sdk.AccAddress, aBalance int, bBalance int) {
-	s.fundAccountBalancesDec(account, NewDec(aBalance), NewDec(bBalance))
-}
-
-func (s *MsgServerLimitTestSuite) fundAliceBalances(a int, b int) {
-	s.fundAccountBalances(s.alice, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) fundAliceBalancesDec(a sdk.Dec, b sdk.Dec) {
-	s.fundAccountBalancesDec(s.alice, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) fundBobBalances(a int, b int) {
-	s.fundAccountBalances(s.bob, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) fundBobBalancesDec(a sdk.Dec, b sdk.Dec) {
-	s.fundAccountBalancesDec(s.bob, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) assertAccountBalances(account sdk.AccAddress, aBalance int, bBalance int) {
-	s.assertAccountBalancesDec(account, NewDec(aBalance), NewDec(bBalance))
-}
-
-func (s *MsgServerLimitTestSuite) assertAccountBalancesDec(
-	account sdk.AccAddress,
-	aBalance sdk.Dec,
-	bBalance sdk.Dec,
-) {
-	aActual := s.app.BankKeeper.GetBalance(s.ctx, account, "TokenA")
-	aDec := sdk.NewDecFromBigIntWithPrec(aActual.Amount.BigInt(), 18)
-	s.Assert().Equal(aBalance, aDec)
-
-	bActual := s.app.BankKeeper.GetBalance(s.ctx, account, "TokenB")
-	bDec := sdk.NewDecFromBigIntWithPrec(bActual.Amount.BigInt(), 18)
-	s.Assert().Equal(bBalance, bDec)
-}
-
-func (s *MsgServerLimitTestSuite) assertAliceBalances(a int, b int) {
-	s.assertAccountBalances(s.alice, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) assertBobBalances(a int, b int) {
-	s.assertAccountBalances(s.bob, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) assertAliceBalancesDec(a sdk.Dec, b sdk.Dec) {
-	s.assertAccountBalancesDec(s.alice, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) assertBobBalancesDec(a sdk.Dec, b sdk.Dec) {
-	s.assertAccountBalancesDec(s.bob, a, b)
-}
-
-func (s *MsgServerLimitTestSuite) assertDexBalances(a int, b int) {
-	s.assertAccountBalances(s.app.AccountKeeper.GetModuleAddress("dex"), a, b)
-}
-
-func (s *MsgServerLimitTestSuite) alicePlacesLimitOrder(wantsToken string, tick int, amountIn int) {
-	s.placeLimitOrder(s.alice, wantsToken, tick, amountIn)
-}
-
-func (s *MsgServerLimitTestSuite) bobPlacesLimitOrder(wantsToken string, tick int, amountIn int) {
-	s.placeLimitOrder(s.bob, wantsToken, tick, amountIn)
-}
-
-func (s *MsgServerLimitTestSuite) placeLimitOrder(account sdk.AccAddress, wantsToken string, tick int, amountIn int) {
-	var tokenIn string
-	if wantsToken == "TokenA" {
-		tokenIn = "TokenB"
-	} else {
-		tokenIn = "TokenA"
-	}
-	amountInDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountIn)))
-	_, err := s.msgServer.PlaceLimitOrder(s.goCtx, &types.MsgPlaceLimitOrder{
-		Creator:   account.String(),
-		Receiver:  account.String(),
-		TokenA:    "TokenA",
-		TokenB:    "TokenB",
-		TickIndex: int64(tick),
-		TokenIn:   tokenIn,
-		AmountIn:  amountInDec,
-	})
-	s.Assert().Nil(err)
-}
-
-type Deposit struct {
-	AmountA   sdk.Dec
-	AmountB   sdk.Dec
-	TickIndex int64
-	FeeIndex  uint64
-}
-
-func NewDeposit(amountA int, amountB int, tickIndex int, feeIndex int) *Deposit {
-	return &Deposit{
-		AmountA:   sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountA))),
-		AmountB:   sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountB))),
-		TickIndex: int64(tickIndex),
-		FeeIndex:  uint64(feeIndex),
-	}
-}
-
-func (s *MsgServerLimitTestSuite) aliceDeposits(deposits ...*Deposit) {
-	s.deposits(s.alice, deposits...)
-}
-
-func (s *MsgServerLimitTestSuite) bobDeposits(deposits ...*Deposit) {
-	s.deposits(s.bob, deposits...)
-}
-
-func (s *MsgServerLimitTestSuite) deposits(account sdk.AccAddress, deposits ...*Deposit) {
-	amountsA := make([]sdk.Dec, len(deposits))
-	amountsB := make([]sdk.Dec, len(deposits))
-	tickIndicies := make([]int64, len(deposits))
-	feeIndexes := make([]uint64, len(deposits))
-	for i, e := range deposits {
-		amountsA[i] = e.AmountA
-		amountsB[i] = e.AmountB
-		tickIndicies[i] = e.TickIndex
-		feeIndexes[i] = e.FeeIndex
-	}
-
-	_, err := s.msgServer.Deposit(s.goCtx, &types.MsgDeposit{
-		Creator:     account.String(),
-		Receiver:    account.String(),
-		TokenA:      "TokenA",
-		TokenB:      "TokenB",
-		AmountsA:    amountsA,
-		AmountsB:    amountsB,
-		TickIndexes: tickIndicies,
-		FeeIndexes:  feeIndexes,
-	})
-	s.Assert().Nil(err)
-}
-
-func (s *MsgServerLimitTestSuite) aliceCancelsLimitOrder(keyToken string, tick int, key int, sharesOut int) {
-	s.cancelsLimitOrder(s.alice, keyToken, tick, key, sharesOut)
-}
-
-func (s *MsgServerLimitTestSuite) cancelsLimitOrder(account sdk.AccAddress, keyToken string, tick int, key int, sharesOut int) {
-	sharesOutDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(sharesOut)))
-	_, err := s.msgServer.CancelLimitOrder(s.goCtx, &types.MsgCancelLimitOrder{
-		Creator:   account.String(),
-		Receiver:  account.String(),
-		TokenA:    "TokenA",
-		TokenB:    "TokenB",
-		TickIndex: int64(tick),
-		KeyToken:  keyToken,
-		Key:       uint64(key),
-		SharesOut: sharesOutDec,
-	})
-	s.Assert().Nil(err)
-}
-
-func (s *MsgServerLimitTestSuite) bobPlacesSwapOrder(wantsToken string, amountIn int, minOut int) {
-	s.placesSwapOrder(s.bob, wantsToken, amountIn, minOut)
-}
-
-func (s *MsgServerLimitTestSuite) placesSwapOrder(account sdk.AccAddress, wantsToken string, amountIn int, minOut int) {
-	var tokenIn string
-	if wantsToken == "TokenA" {
-		tokenIn = "TokenB"
-	} else {
-		tokenIn = "TokenA"
-	}
-	amountInDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountIn)))
-	minOutDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(minOut)))
-	_, err := s.msgServer.Swap(s.goCtx, &types.MsgSwap{
-		Creator:  account.String(),
-		Receiver: account.String(),
-		TokenA:   "TokenA",
-		TokenB:   "TokenB",
-		TokenIn:  tokenIn,
-		AmountIn: amountInDec,
-		MinOut:   minOutDec,
-	})
-	s.Assert().Nil(err)
-}
-
-func (s *MsgServerLimitTestSuite) aliceWithdrawsFilledLimitOrder(withdrawToken string, tick int) {
-	s.withdrawsFilledLimitOrder(s.alice, withdrawToken, tick)
-}
-
-func (s *MsgServerLimitTestSuite) withdrawsFilledLimitOrder(account sdk.AccAddress, withdrawToken string, tick int) {
-	_, err := s.msgServer.WithdrawFilledLimitOrder(s.goCtx, &types.MsgWithdrawFilledLimitOrder{
-		Creator:   account.String(),
-		Receiver:  account.String(),
-		TokenA:    "TokenA",
-		TokenB:    "TokenB",
-		TickIndex: int64(tick),
-		KeyToken:  withdrawToken,
-		Key:       0,
-	})
-	s.Assert().Nil(err)
-}
-
-func (s *MsgServerLimitTestSuite) traceBalances() {
-	aliceA := s.app.BankKeeper.GetBalance(s.ctx, s.alice, "TokenA")
-	aliceB := s.app.BankKeeper.GetBalance(s.ctx, s.alice, "TokenB")
-	bobA := s.app.BankKeeper.GetBalance(s.ctx, s.bob, "TokenA")
-	bobB := s.app.BankKeeper.GetBalance(s.ctx, s.bob, "TokenB")
-	fmt.Printf("Alice: %+v %+v, Bob: %+v %+v", aliceA, aliceB, bobA, bobB)
 }
--- x/dex/keeper/msg_server_test.go
+++ x/dex/keeper/msg_server_test.go
@@ -2,15 +2,277 @@ package keeper_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
-	keepertest "github.com/NicholasDotSol/duality/testutil/keeper"
+	dualityapp "github.com/NicholasDotSol/duality/app"
 	"github.com/NicholasDotSol/duality/x/dex/keeper"
+	. "github.com/NicholasDotSol/duality/x/dex/keeper/internal/testutils"
 	"github.com/NicholasDotSol/duality/x/dex/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	k, ctx := keepertest.DexKeeper(t)
-	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+type MsgServerTestSuite struct {
+	suite.Suite
+	app         *dualityapp.App
+	msgServer   types.MsgServer
+	ctx         sdk.Context
+	queryClient types.QueryClient
+	alice       sdk.AccAddress
+	bob         sdk.AccAddress
+	goCtx       context.Context
+}
+
+func TestMsgServerLimitTestSuite(t *testing.T) {
+	suite.Run(t, new(MsgServerTestSuite))
+}
+
+func (s *MsgServerTestSuite) SetupTest() {
+	app := dualityapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
+	app.BankKeeper.SetParams(ctx, banktypes.DefaultParams())
+
+	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, app.DexKeeper)
+	queryClient := types.NewQueryClient(queryHelper)
+
+	accAlice := app.AccountKeeper.NewAccountWithAddress(ctx, s.alice)
+	app.AccountKeeper.SetAccount(ctx, accAlice)
+	accBob := app.AccountKeeper.NewAccountWithAddress(ctx, s.bob)
+	app.AccountKeeper.SetAccount(ctx, accBob)
+
+	// Set Fee List
+	app.DexKeeper.AppendFeeList(ctx, types.FeeList{0, 1})
+	app.DexKeeper.AppendFeeList(ctx, types.FeeList{1, 2})
+	app.DexKeeper.AppendFeeList(ctx, types.FeeList{2, 3})
+	app.DexKeeper.AppendFeeList(ctx, types.FeeList{3, 4})
+
+	s.app = app
+	s.msgServer = keeper.NewMsgServerImpl(app.DexKeeper)
+	s.ctx = ctx
+	s.goCtx = sdk.WrapSDKContext(ctx)
+	s.queryClient = queryClient
+	s.alice = sdk.AccAddress([]byte("alice"))
+	s.bob = sdk.AccAddress([]byte("bob"))
+}
+
+func (s *MsgServerTestSuite) fundAccountBalancesDec(account sdk.AccAddress, aBalance sdk.Dec, bBalance sdk.Dec) {
+	aBalanceInt := sdk.NewIntFromBigInt(aBalance.BigInt())
+	bBalanceInt := sdk.NewIntFromBigInt(bBalance.BigInt())
+	balances := sdk.NewCoins(NewACoin(aBalanceInt), NewBCoin(bBalanceInt))
+	err := simapp.FundAccount(s.app.BankKeeper, s.ctx, account, balances)
+	s.Assert().NoError(err)
+	s.assertAccountBalancesDec(account, aBalance, bBalance)
+}
+
+func (s *MsgServerTestSuite) fundAccountBalances(account sdk.AccAddress, aBalance int, bBalance int) {
+	s.fundAccountBalancesDec(account, NewDec(aBalance), NewDec(bBalance))
+}
+
+func (s *MsgServerTestSuite) fundAliceBalances(a int, b int) {
+	s.fundAccountBalances(s.alice, a, b)
+}
+
+func (s *MsgServerTestSuite) fundAliceBalancesDec(a sdk.Dec, b sdk.Dec) {
+	s.fundAccountBalancesDec(s.alice, a, b)
+}
+
+func (s *MsgServerTestSuite) fundBobBalances(a int, b int) {
+	s.fundAccountBalances(s.bob, a, b)
+}
+
+func (s *MsgServerTestSuite) fundBobBalancesDec(a sdk.Dec, b sdk.Dec) {
+	s.fundAccountBalancesDec(s.bob, a, b)
+}
+
+func (s *MsgServerTestSuite) assertAccountBalances(account sdk.AccAddress, aBalance int, bBalance int) {
+	s.assertAccountBalancesDec(account, NewDec(aBalance), NewDec(bBalance))
+}
+
+func (s *MsgServerTestSuite) assertAccountBalancesDec(
+	account sdk.AccAddress,
+	aBalance sdk.Dec,
+	bBalance sdk.Dec,
+) {
+	aActual := s.app.BankKeeper.GetBalance(s.ctx, account, "TokenA")
+	aDec := sdk.NewDecFromBigIntWithPrec(aActual.Amount.BigInt(), 18)
+	s.Assert().Equal(aBalance, aDec)
+
+	bActual := s.app.BankKeeper.GetBalance(s.ctx, account, "TokenB")
+	bDec := sdk.NewDecFromBigIntWithPrec(bActual.Amount.BigInt(), 18)
+	s.Assert().Equal(bBalance, bDec)
+}
+
+func (s *MsgServerTestSuite) assertAliceBalances(a int, b int) {
+	s.assertAccountBalances(s.alice, a, b)
+}
+
+func (s *MsgServerTestSuite) assertBobBalances(a int, b int) {
+	s.assertAccountBalances(s.bob, a, b)
+}
+
+func (s *MsgServerTestSuite) assertAliceBalancesDec(a sdk.Dec, b sdk.Dec) {
+	s.assertAccountBalancesDec(s.alice, a, b)
+}
+
+func (s *MsgServerTestSuite) assertBobBalancesDec(a sdk.Dec, b sdk.Dec) {
+	s.assertAccountBalancesDec(s.bob, a, b)
+}
+
+func (s *MsgServerTestSuite) assertDexBalances(a int, b int) {
+	s.assertAccountBalances(s.app.AccountKeeper.GetModuleAddress("dex"), a, b)
+}
+
+func (s *MsgServerTestSuite) alicePlacesLimitOrder(wantsToken string, tick int, amountIn int) {
+	s.placesLimitOrder(s.alice, wantsToken, tick, amountIn)
+}
+
+func (s *MsgServerTestSuite) bobPlacesLimitOrder(wantsToken string, tick int, amountIn int) {
+	s.placesLimitOrder(s.bob, wantsToken, tick, amountIn)
+}
+
+func (s *MsgServerTestSuite) placesLimitOrder(account sdk.AccAddress, wantsToken string, tick int, amountIn int) {
+	var tokenIn string
+	if wantsToken == "TokenA" {
+		tokenIn = "TokenB"
+	} else {
+		tokenIn = "TokenA"
+	}
+	amountInDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountIn)))
+	_, err := s.msgServer.PlaceLimitOrder(s.goCtx, &types.MsgPlaceLimitOrder{
+		Creator:   account.String(),
+		Receiver:  account.String(),
+		TokenA:    "TokenA",
+		TokenB:    "TokenB",
+		TickIndex: int64(tick),
+		TokenIn:   tokenIn,
+		AmountIn:  amountInDec,
+	})
+	s.Assert().Nil(err)
+}
+
+type Deposit struct {
+	AmountA   sdk.Dec
+	AmountB   sdk.Dec
+	TickIndex int64
+	FeeIndex  uint64
+}
+
+func NewDeposit(amountA int, amountB int, tickIndex int, feeIndex int) *Deposit {
+	return &Deposit{
+		AmountA:   sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountA))),
+		AmountB:   sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountB))),
+		TickIndex: int64(tickIndex),
+		FeeIndex:  uint64(feeIndex),
+	}
+}
+
+func (s *MsgServerTestSuite) aliceDeposits(deposits ...*Deposit) {
+	s.deposits(s.alice, deposits...)
+}
+
+func (s *MsgServerTestSuite) bobDeposits(deposits ...*Deposit) {
+	s.deposits(s.bob, deposits...)
+}
+
+func (s *MsgServerTestSuite) deposits(account sdk.AccAddress, deposits ...*Deposit) {
+	amountsA := make([]sdk.Dec, len(deposits))
+	amountsB := make([]sdk.Dec, len(deposits))
+	tickIndicies := make([]int64, len(deposits))
+	feeIndexes := make([]uint64, len(deposits))
+	for i, e := range deposits {
+		amountsA[i] = e.AmountA
+		amountsB[i] = e.AmountB
+		tickIndicies[i] = e.TickIndex
+		feeIndexes[i] = e.FeeIndex
+	}
+
+	_, err := s.msgServer.Deposit(s.goCtx, &types.MsgDeposit{
+		Creator:     account.String(),
+		Receiver:    account.String(),
+		TokenA:      "TokenA",
+		TokenB:      "TokenB",
+		AmountsA:    amountsA,
+		AmountsB:    amountsB,
+		TickIndexes: tickIndicies,
+		FeeIndexes:  feeIndexes,
+	})
+	s.Assert().Nil(err)
+}
+
+func (s *MsgServerTestSuite) aliceCancelsLimitOrder(keyToken string, tick int, key int, sharesOut int) {
+	s.cancelsLimitOrder(s.alice, keyToken, tick, key, sharesOut)
+}
+
+func (s *MsgServerTestSuite) cancelsLimitOrder(account sdk.AccAddress, keyToken string, tick int, key int, sharesOut int) {
+	sharesOutDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(sharesOut)))
+	_, err := s.msgServer.CancelLimitOrder(s.goCtx, &types.MsgCancelLimitOrder{
+		Creator:   account.String(),
+		Receiver:  account.String(),
+		TokenA:    "TokenA",
+		TokenB:    "TokenB",
+		TickIndex: int64(tick),
+		KeyToken:  keyToken,
+		Key:       uint64(key),
+		SharesOut: sharesOutDec,
+	})
+	s.Assert().Nil(err)
+}
+
+func (s *MsgServerTestSuite) bobPlacesSwapOrder(wantsToken string, amountIn int, minOut int) {
+	s.placesSwapOrder(s.bob, wantsToken, amountIn, minOut)
+}
+
+func (s *MsgServerTestSuite) placesSwapOrder(account sdk.AccAddress, wantsToken string, amountIn int, minOut int) {
+	var tokenIn string
+	if wantsToken == "TokenA" {
+		tokenIn = "TokenB"
+	} else {
+		tokenIn = "TokenA"
+	}
+	amountInDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(amountIn)))
+	minOutDec := sdk.NewDecFromInt(sdk.NewIntFromUint64(uint64(minOut)))
+	_, err := s.msgServer.Swap(s.goCtx, &types.MsgSwap{
+		Creator:  account.String(),
+		Receiver: account.String(),
+		TokenA:   "TokenA",
+		TokenB:   "TokenB",
+		TokenIn:  tokenIn,
+		AmountIn: amountInDec,
+		MinOut:   minOutDec,
+	})
+	s.Assert().Nil(err)
+}
+
+func (s *MsgServerTestSuite) aliceWithdrawsFilledLimitOrder(withdrawToken string, tick int) {
+	s.withdrawsFilledLimitOrder(s.alice, withdrawToken, tick)
+}
+
+func (s *MsgServerTestSuite) withdrawsFilledLimitOrder(account sdk.AccAddress, withdrawToken string, tick int) {
+	_, err := s.msgServer.WithdrawFilledLimitOrder(s.goCtx, &types.MsgWithdrawFilledLimitOrder{
+		Creator:   account.String(),
+		Receiver:  account.String(),
+		TokenA:    "TokenA",
+		TokenB:    "TokenB",
+		TickIndex: int64(tick),
+		KeyToken:  withdrawToken,
+		Key:       0,
+	})
+	s.Assert().Nil(err)
+}
+
+func (s *MsgServerTestSuite) traceBalances() {
+	aliceA := s.app.BankKeeper.GetBalance(s.ctx, s.alice, "TokenA")
+	aliceB := s.app.BankKeeper.GetBalance(s.ctx, s.alice, "TokenB")
+	bobA := s.app.BankKeeper.GetBalance(s.ctx, s.bob, "TokenA")
+	bobB := s.app.BankKeeper.GetBalance(s.ctx, s.bob, "TokenB")
+	fmt.Printf("Alice: %+v %+v, Bob: %+v %+v", aliceA, aliceB, bobA, bobB)
 }
