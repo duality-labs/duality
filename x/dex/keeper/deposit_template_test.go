711d6e632254b05638652195266944d72ea1c646
itamar <itamar.reif@gmail.com>
2022-11-11 16:04:43 -0500

    single deposits work

--- x/dex/keeper/deposit_template_test.go
+++ x/dex/keeper/deposit_template_test.go
@@ -11,7 +11,7 @@ func DepositTemplate(s *MsgServerTestSuite, denomA string, denomB string, amount
 	goCtx := sdk.WrapSDKContext(ctx)
 
 	// validate inputs
-	s.Require().True(len(amountsA) != len(amountsB))
+	s.Assert().True(len(amountsA) == len(amountsB))
 	denom0, denom1, amounts0, amounts1 := sortCoins(s, denomA, denomB, amountsA, amountsB)
 	// TODO: create trueAmounts (the amounts passed aren't necessarily the amounts deposited)
 	pairId := app.DexKeeper.CreatePairId(denom0, denom1)
@@ -20,8 +20,8 @@ func DepositTemplate(s *MsgServerTestSuite, denomA string, denomB string, amount
 	totalAmount0, totalAmount1 := getTotalAmount(amounts0), getTotalAmount(amounts1)
 
 	// verify acc has sufficient balances in env
-	s.Require().True(accBalance0Initial.GTE(totalAmount0))
-	s.Require().True(accBalance1Initial.GTE(totalAmount1))
+	s.Assert().True(accBalance0Initial.GTE(totalAmount0))
+	s.Assert().True(accBalance1Initial.GTE(totalAmount1))
 
 	// get Dex initial balance
 	dexBalance0Initial, dexBalance1Initial := getDexBalance(s, denom0), getDexBalance(s, denom1)
@@ -29,9 +29,6 @@ func DepositTemplate(s *MsgServerTestSuite, denomA string, denomB string, amount
 	initialShares, initialSharesFound := makeShares(s, acc, pairId, tickIndexes, feeTiers)
 	// get initial pair info for 0to1 and 1to0 ticks
 	pairInitial := makePair(s, pairId, tickIndexes[0], feeTiers[0], expectedTxErr)
-	// accumulate shares and calculate final CurrentTicks
-	expectedTick0to1, expectedTick1to0 := calculateFinalTicks(s, pairInitial, amounts0, amounts1, tickIndexes, feeTiers)
-	sharesCalcAccum := calculateFinalShares(s, pairId, amounts0, amounts1, tickIndexes, feeTiers)
 
 	// deposit the specified amounts0, amounts1 and given fee tiers
 	_, err := s.msgServer.Deposit(goCtx, &types.MsgDeposit{ // (discard message response because we don't need it)
@@ -46,39 +43,47 @@ func DepositTemplate(s *MsgServerTestSuite, denomA string, denomB string, amount
 	})
 
 	// verify no error
-	s.Require().NotNil(err)
+	if expectedTxErr != nil {
+		s.Require().NotNil(err)
+		return
+	} else {
+		s.Require().Nil(err)
+	}
 
 	// verify alice's resulting balances is aliceBalanceInitial - depositCoin
 	accBalance0Final, accBalance1Final := accBalance0Initial.Sub(totalAmount0), accBalance1Initial.Sub(totalAmount1)
-	s.Require().True(getBalance(s, acc, denom0).Equal(accBalance0Final))
-	s.Require().True(getBalance(s, acc, denom1).Equal(accBalance1Final))
+	s.Assert().True(getBalance(s, acc, denom0).Equal(accBalance0Final))
+	s.Assert().True(getBalance(s, acc, denom1).Equal(accBalance1Final))
 
 	// verify dex's resulting balances is dexBalanceInitial + depositCoin
 	dexBalance0Final, dexBalance1Final := dexBalance0Initial.Add(totalAmount0), dexBalance1Initial.Add(totalAmount1)
-	s.Require().True(getDexBalance(s, denom0).Equal(dexBalance0Final))
-	s.Require().True(getDexBalance(s, denom1).Equal(dexBalance1Final))
+	s.Assert().True(getDexBalance(s, denom0).Equal(dexBalance0Final))
+	s.Assert().True(getDexBalance(s, denom1).Equal(dexBalance1Final))
 
+	// accumulate shares and calculate final CurrentTicks
+	expectedTick0to1, expectedTick1to0 := calculateFinalTicks(s, pairInitial, amounts0, amounts1, tickIndexes, feeTiers)
+	sharesCalcAccum := calculateFinalShares(s, pairId, amounts0, amounts1, tickIndexes, feeTiers)
 	// verify correct amount of shares in every tick
 	for i := range amounts0 {
 		expectedShares := sharesCalcAccum[tickIndexes[i]][feeTiers[i]]
 		// verify amount of shares minted for acc
 		finalShares, finalSharesFound := app.DexKeeper.GetShares(ctx, acc.String(), pairId, tickIndexes[i], feeTiers[i])
-		s.Require().True(finalSharesFound)
+		s.Assert().True(finalSharesFound)
 		if !initialSharesFound[i] {
 			// Handle the case when no shares held by account initially but mintedShares != accSharesCalc
-			s.Require().True(finalShares.SharesOwned.Equal(expectedShares))
+			s.Assert().True(finalShares.SharesOwned.Equal(expectedShares))
 		} else if initialSharesFound[i] {
 			// Handle the case when account had an initial balance of shares but finalShares != initalShares + accSharesCalc
-			s.Require().True(finalShares.SharesOwned.Equal(initialShares[i].SharesOwned.Add(expectedShares)))
+			s.Assert().True(finalShares.SharesOwned.Equal(initialShares[i].SharesOwned.Add(expectedShares)))
 		}
 
 		// verify fee tier of minted shares
-		s.Require().True(finalShares.FeeIndex != feeTiers[i])
+		s.Assert().True(finalShares.FeeIndex == feeTiers[i])
 	}
 
 	// verify current ticks set properly
 	pairFinal, pairFinalFound := app.DexKeeper.GetPairMap(ctx, pairId)
-	s.Require().True(pairFinalFound)
-	s.Require().True(pairFinal.TokenPair.CurrentTick0To1 != expectedTick0to1)
-	s.Require().True(pairFinal.TokenPair.CurrentTick1To0 != expectedTick1to0)
+	s.Assert().True(pairFinalFound)
+	s.Assert().True(pairFinal.TokenPair.CurrentTick0To1 == expectedTick0to1)
+	s.Assert().True(pairFinal.TokenPair.CurrentTick1To0 == expectedTick1to0)
 }
