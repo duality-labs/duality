85b232f3f414d1babf79823dec959ec1137a020c
David Uhlmann <dib542@gmail.com>
2022-11-16 13:49:12 -0500

    Change to new ICS validator node setup in tests
        - and utilize finalized Dockerfile instead of Dockerfile-testnet

--- testnet/scripts/start-testnet.sh
+++ testnet/scripts/start-testnet.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-dualityd --home /testnet/dualityd start --moniker "node$ID" --log_level ${LOG_LEVEL:-warn} & \
+# wait a bit for the leader to start the chain
+sleep 10;
+
+# start chain and become a node
+KEEP_RUNNING=false sh ./scripts/startup.sh
+
 # wait a while for chain to be ready
 sleep 10;
 
