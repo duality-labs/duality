94877e15b8d78a4c1cd6c4d56111d15b64a3dcee
Elijah Fox <elijahf@umich.edu>
2022-11-17 14:14:59 -0500

    fixing updates on withdrawal

--- x/dex/keeper/core_helper.go
+++ x/dex/keeper/core_helper.go
@@ -461,7 +461,7 @@ func (k Keeper) CalcTickPointersPostRemoveToken0(goCtx context.Context, pair *ty
 	// We have removed all of Token0 from the pool
 	if tickIndex == *minTick && tickIndex == *cur1To0{
 		*minTick = math.MaxInt64
-		// we leave cur1To0 where it is because otherwise we lose the last traded price
+		*cur1To0 = math.MaxInt64
 	} else if tickIndex == *minTick {
 		// TODO: We should really search for the next minTick but this introduces a
 		// vulnerability unless we have a dedicated data structure for avoiding.
@@ -504,7 +504,7 @@ func (k Keeper) CalcTickPointersPostRemoveToken1(goCtx context.Context, pair *ty
 	// only need to act when the token is exhausted at one of the bounds
 	if tickIndex == *maxTick && tickIndex == *cur0To1{
 		*maxTick = math.MinInt64
-		// we leave cur0To1 where it is because otherwise we lose the last traded price
+		*cur0To1 = math.MinInt64
 	} else if tickIndex == *maxTick{
 		// TODO: We should really search for the next maxTick but this introduces a
 		// vulnerability unless we have a dedicated data structure for avoiding.
--- x/dex/keeper/withdrawCore_unit_test.go
+++ x/dex/keeper/withdrawCore_unit_test.go
@@ -58,3 +58,50 @@ func (s *MsgServerTestSuite) TestPartialWithdrawOnlyB() {
 	s.assertCurr1To0(math.MaxInt64)
 	s.assertCurr0To1(1)
 }
+
+func (s *MsgServerTestSuite) TestFullWithdrawOnlyB() {
+	s.fundAliceBalances(50, 50)
+	// CASE
+	// Alice deposits 10 of B at tick 0, fee tier 0
+	// and then withdraws 10 shares of B
+
+	// DATA
+	// Alice should be credited 10 total shares
+	// Shares = amount0 + price1to0 * amount1
+	// Shares = 10 + 0 * 0 = 10
+	s.aliceDeposits(NewDeposit(0, 10, 0, 0))
+
+	s.assertAliceBalances(50, 40)
+	s.assertDexBalances(0, 10)
+	s.assertCurr1To0(math.MaxInt64)
+	s.assertCurr0To1(1)
+
+	s.aliceWithdraws(NewWithdrawl(10, 0, 0))
+
+	s.assertAliceBalances(50, 50)
+	s.assertDexBalances(0, 0)
+	s.assertCurr1To0(math.MaxInt64)
+	s.assertCurr0To1(math.MinInt64)
+}
+
+func (s *MsgServerTestSuite) TestCurrentTickUpdatesAfterOneSidedDeposit() {
+	s.fundAliceBalances(50, 50)
+	// CASE
+	// Alice deposits 10 of A and B with a spread (fee) of +- 3 ticks
+	// Alice then deposits 10 A with a spread (fee) of -1 ticks
+	// Since there is no B in Alice's second deposit, the current tick shouldn't update
+
+	s.aliceDeposits(NewDeposit(10, 10, 0, 1))
+
+	s.assertAliceBalances(40, 40)
+	s.assertDexBalances(10, 10)
+	s.assertCurr1To0(-3)
+	s.assertCurr0To1(3)
+
+	s.aliceDeposits(NewDeposit(10, 0, 0, 0))
+
+	s.assertAliceBalances(30, 40)
+	s.assertDexBalances(20, 10)
+	s.assertCurr1To0(-1)
+	s.assertCurr0To1(3)
+}
