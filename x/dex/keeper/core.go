e73398228c68aa9f5bd3830823cfb71f13b5809c
jtieri <justin@thetieris.com>
2022-11-18 13:03:04 -0600

    debug: add debug output for failing deposits

--- x/dex/keeper/core.go
+++ x/dex/keeper/core.go
@@ -101,8 +101,13 @@ func (k Keeper) DepositCore(
 		k.SetPairMap(ctx, pair)
 		k.SetTickMap(ctx, pairId, lowerTick)
 		k.SetTickMap(ctx, pairId, upperTick)
+
+		k.Logger(ctx).Error("AFTER SET PAIR AND TICK MAP")
+
 		k.UpdateTickPointersPostAddToken0(goCtx, &pair, &lowerTick)
 		k.UpdateTickPointersPostAddToken1(goCtx, &pair, &upperTick)
+
+		k.Logger(ctx).Error("AFTER UPDATE TICK POINTERS")
 
 		amounts0Deposited[i] = trueAmount0
 		amounts1Deposited[i] = trueAmount1
