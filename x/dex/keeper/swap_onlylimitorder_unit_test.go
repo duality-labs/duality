6aacaf5ba880166deaed28c735e44e303c5d1759
itamar <itamar.reif@gmail.com>
2022-11-21 18:54:26 -0500

    fully exhaust LO test

--- x/dex/keeper/swap_onlylimitorder_unit_test.go
+++ x/dex/keeper/swap_onlylimitorder_unit_test.go
@@ -360,6 +360,43 @@ func (s *MsgServerTestSuite) TestSwapOnlyLOPartiallyFilledCorrectExecution() {
 	// s.assertLimitLiquidityAtTickDec("TokenB", 1, NewDec(20).Sub(amountOutSetup).Sub(expectedAmountOut))
 }
 
+func (s *MsgServerTestSuite) TestSwapOnlyLOExhaustLOCorrectExecution() {
+	// TODO: this fails due to fill and place key bug
+	s.fundAliceBalances(50, 50)
+	s.fundBobBalances(50, 0)
+	// GIVEN
+	// place LO selling 10 of token B at tick 1
+	s.aliceLimitSells("TokenB", 1, 10)
+
+	// Partially fill the LO, will have some token B remaining to fill
+	s.bobMarketSells("TokenA", 5, 4)
+	// in 5.000499950004999500, out 4.999500049995000500
+	expectedAmountLeftSetup, amountOutSetup := s.calculateSingleSwapOnlyLOAToB(1, NewDec(10), NewDec(5))
+	amountInSetup := NewDec(5).Sub(expectedAmountLeftSetup)
+	s.assertLimitLiquidityAtTickDec("TokenB", 1, NewDec(10).Sub(amountOutSetup))
+
+	// place another LO selling 10 of token B at tick 1
+	s.aliceLimitSells("TokenB", 1, 10)
+	// TODO: uncomment
+	// s.assertLimitLiquidityAtTickDec("TokenB", 1, NewDec(20).Sub(amountOutSetup))
+	bobBalanceSetupB := NewDec(50).Sub(amountInSetup)
+	s.assertBobBalancesDec(bobBalanceSetupB, amountOutSetup)
+
+	// WHEN
+	// swap 5 of token A for B with minOut 4
+	amountIn, amountInDec := 20, NewDec(20)
+	s.bobMarketSells("TokenA", amountIn, 0)
+
+	// THEN
+	// swap should have in out
+	expectedAmountLeft, expectedAmountOut := s.calculateSingleSwapOnlyLOAToB(1, NewDec(10), amountInDec)
+	expectedAmountIn := amountInDec.Sub(expectedAmountLeft)
+	s.assertBobBalancesDec(bobBalanceSetupB.Sub(expectedAmountIn), amountOutSetup.Add(expectedAmountOut))
+	s.assertDexBalancesDec(expectedAmountIn.Add(amountInSetup), NewDec(20).Sub(amountOutSetup).Sub(expectedAmountOut))
+	// TODO: uncomment
+	// s.assertLimitLiquidityAtTickDec("TokenB", 1, NewDec(20).Sub(amountOutSetup).Sub(expectedAmountOut))
+}
+
 func (s *MsgServerTestSuite) TestSwapOnlyLOPartiallyFilled0to1DoesntMove0to1() {
 	s.fundAliceBalances(50, 50)
 	s.fundBobBalances(50, 0)
