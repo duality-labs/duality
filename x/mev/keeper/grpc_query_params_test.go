688b6c4ced1281ad1d55e9248783f17c9dd094d8
finkbeca <colinfinkbeiner1@gmail.com>
2022-12-27 16:50:12 -0500

    more name changes to mev

--- x/mev/keeper/grpc_query_params_test.go
+++ x/mev/keeper/grpc_query_params_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestParamsQuery(t *testing.T) {
-	keeper, ctx := testkeeper.MevdummyKeeper(t)
+	keeper, ctx := testkeeper.MevKeeper(t)
 	wctx := sdk.WrapSDKContext(ctx)
 	params := types.DefaultParams()
 	keeper.SetParams(ctx, params)
