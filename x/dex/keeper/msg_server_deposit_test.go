b90f8e36bf71db1a262e8bfe8c5c00bc0846952b
itamar <itamar.reif@gmail.com>
2023-01-09 19:04:59 -0500

    remove redundant tests

--- x/dex/keeper/msg_server_deposit_test.go
+++ x/dex/keeper/msg_server_deposit_test.go
@@ -3,29 +3,7 @@ package keeper_test
 import (
 	"github.com/NicholasDotSol/duality/x/dex/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
-
-func (s *MsgServerTestSuite) TestSingleDepositDepositAmountZero() {
-	s.fundAliceBalances(25, 25)
-	s.fundBobBalances(25, 25)
-
-	_, err := s.msgServer.Deposit(s.goCtx, &types.MsgDeposit{
-		Creator:     s.alice.String(),
-		Receiver:    s.alice.String(),
-		TokenA:      "TokenA",
-		TokenB:      "TokenB",
-		AmountsA:    []sdk.Int{sdk.NewInt(0)},
-		AmountsB:    []sdk.Int{sdk.NewInt(0)},
-		TickIndexes: []int64{0},
-		FeeIndexes:  []uint64{1},
-	})
-	s.Assert().ErrorIs(err, sdkerrors.ErrInvalidType)
-
-	s.assertAliceBalances(25, 25)
-	s.assertDexBalances(0, 0)
-
-}
 
 func (s *MsgServerTestSuite) TestSingleDepositDepositFail() {
 	s.fundAliceBalances(25, 25)
--- x/dex/keeper/msg_server_withdrawl_test.go
+++ x/dex/keeper/msg_server_withdrawl_test.go
@@ -3,122 +3,11 @@ package keeper_test
 import (
 	"math"
 
-	"github.com/NicholasDotSol/duality/x/dex/keeper"
 	"github.com/NicholasDotSol/duality/x/dex/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // ** CORE TESTS **
-func (s *MsgServerTestSuite) TestSingleWithdrawlFull() {
-	s.fundAliceBalances(100, 50)
-
-	// IF Alice deposits 100TokenA & 100TokenB into tick 0 @ feeTier 0
-	s.aliceDeposits(NewDeposit(100, 50, 0, 0))
-
-	// WHEN Alice withdraws her entire position (150 Shares)
-	err := s.aliceWithdraws(NewWithdrawl(150, 0, 0))
-	s.Assert().NoError(err)
-
-	// THEN assert alice gets 100TokenA & 150TokenB; Dex retains no balance; No active Ticks
-	s.assertAliceShares(0, 0, 0)
-	s.assertAliceBalances(100, 50)
-	s.assertDexBalances(0, 0)
-}
-
-func (s *MsgServerTestSuite) TestSingleWithdrawlPartial() {
-	s.fundAliceBalances(100, 50)
-
-	// IF Alice deposits 100 TokenA into tick 0 @ feeTier 0
-	s.aliceDeposits(NewDeposit(100, 50, 0, 0))
-
-	// WHEN Alice withdraws her half her position (75 Shares)
-	err := s.aliceWithdraws(NewWithdrawl(75, 0, 0))
-	s.Assert().NoError(err)
-
-	// THEN Alice gets half her deposit back; Dex retains half
-	s.assertAliceShares(0, 0, 75)
-	s.assertAliceBalances(50, 25)
-	s.assertDexBalances(50, 25)
-	// CurrentTick values remain unchanged
-	s.assertCurrentTicks(-1, 1)
-}
-
-func (s *MsgServerTestSuite) TestSingleWithdrawlMaxFee() {
-	s.fundAliceBalances(100, 0)
-
-	// IF Alice deposits 100 TokenA into tick 0 @ feeTier 3
-	s.aliceDeposits(NewDeposit(100, 0, 0, 3))
-
-	// WHEN Alice withdraws her half her position
-	s.aliceWithdraws(NewWithdrawl(50, 0, 3))
-
-	// THEN Alice gets 50 TokenA back and Dex retains balance of 50
-	s.assertAliceShares(0, 3, 50)
-	s.assertAliceBalances(50, 0)
-	s.assertDexBalances(50, 0)
-}
-
-func (s *MsgServerTestSuite) TestSingleWithdrawlSingleSide() {
-	s.fundAliceBalances(100, 0)
-
-	// IF Alice deposits 100 TokenA into tick 0 @ feeTier 0
-	s.aliceDeposits(NewDeposit(100, 0, 0, 0))
-
-	// WHEN Alice withdraws her entire position
-	s.aliceWithdraws(NewWithdrawl(100, 0, 0))
-
-	// THEN Alice gets 100TokenA back; Dex retains no balance; No Active Ticks
-	s.assertAliceShares(0, 0, 0)
-	s.assertAliceBalances(100, 0)
-	s.assertDexBalances(0, 0)
-
-}
-
-func (s *MsgServerTestSuite) TestSingleWithdrawlShiftsTickRight() {
-	s.fundAliceBalances(200, 0)
-
-	// IF Alice deposits 100TokenA into ticks 1 & 2
-	s.aliceDeposits(
-		NewDeposit(100, 0, 2, 0),
-		NewDeposit(100, 0, 1, 0),
-	)
-	// currentTick1To0 = 2
-	s.assertCurrentTicks(1, math.MaxInt64)
-
-	// WHEN Alice withdraws her shares from Tick 1
-	s.aliceWithdraws(NewWithdrawl(100, 2, 0))
-
-	// THEN currentTick1To0 = 3
-	//TODO: this is currently failling because of TickCount bug
-	s.assertCurrentTicks(0, math.MaxInt64)
-}
-
-func (s *MsgServerTestSuite) TestSingleWithdrawlShiftsTickLeft() {
-	s.fundAliceBalances(0, 200)
-
-	// IF Alice deposits 100TokenA into ticks 1 & 2
-	s.aliceDeposits(
-		NewDeposit(0, 100, 1, 0),
-		NewDeposit(0, 100, 2, 0),
-	)
-	// currentTick0To1 = 1
-	s.assertCurrentTicks(math.MinInt64, 2)
-
-	// WHEN Alice withdraws her shares from Tick 1
-	price1To0, err := keeper.CalcPrice1To0(1)
-	if err != nil {
-		panic(err)
-	}
-	sharesToWithdraw := keeper.CalcShares(
-		sdk.NewInt(0),
-		sdk.NewInt(100),
-		price1To0,
-	).TruncateInt()
-	s.aliceWithdraws(NewWithdrawlInt(sharesToWithdraw, 1, 0))
-
-	// THEN currentTick0To1 = 0
-	s.assertCurrentTicks(math.MinInt64, 3)
-}
 
 func (s *MsgServerTestSuite) TestMultiWithdrawlShiftsTickLeft() {
 	s.fundAliceBalances(300, 0)
@@ -139,19 +28,6 @@ func (s *MsgServerTestSuite) TestMultiWithdrawlShiftsTickLeft() {
 
 	s.assertCurrentTicks(0, math.MaxInt64)
 }
-
-// TODO: more test to write
-
-// TestMultiWithdrawlShiftsTickRight
-
-//TestSingleWithdrawTiersShiftsTickRight
-//TestSingleWithdrawTiersShiftsTickLeft
-//TestMultiWithdrawTiersShiftsTickRight
-//TestMultiWithdrawTiersShiftsTickLeft
-
-// d 100 t1 d 100 d 100 t3 t2 [w100 t3 w 50 t2] 100 t2 => currentTick1To0 = 3
-
-// d 100 t1 d 100 d 100 t3 t2 [w100 t2 w100 t3] 100 t2 => currentTick1To0 =
 
 // ** EDGE CASE FAILURE TESTS **
 
