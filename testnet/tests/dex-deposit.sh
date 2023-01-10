cee44d7cf9d079ad2302d2ffb6a9a67847aa0e10
David Uhlmann <dib542@gmail.com>
2023-01-10 21:00:43 +1000

    Fix tests to work against API v0.1.0

--- testnet/tests/dex-deposit.sh
+++ testnet/tests/dex-deposit.sh
@@ -9,13 +9,14 @@ test_name="dex: can make deposit"
 echo "$test_name: setup"
 
 # create new person with funds
+# (token amounts here are measured in utoken denom)
 person=$(bash /root/.duality/scripts/test_helpers.sh createAndFundUser 1000tokenA,1000tokenB)
 
 echo "$test_name: start test"
 # deposit tokens
 # wait for tx to be processed and return an exit code
-# (amounts 0.0000000000000001 are measured in token main denom, whereas when using the bank send function (1000) that is in the small denom which is 1e18x smaller)
-tx_result=$(dualityd tx dex deposit $(dualityd keys show $person --output json | jq -r .address) tokenA tokenB 0.0000000000000001 0.0000000000000001 1 0 --from "$person" --yes --output json --broadcast-mode block)
+# (token amounts here are measured in utoken denom)
+tx_result=$(dualityd tx dex deposit $(dualityd keys show $person --output json | jq -r .address) tokenA tokenB 100 100 1 0 --from "$person" --yes --output json --broadcast-mode block)
 
 # assert that result has no errors
 bash /root/.duality/scripts/test_helpers.sh throwOnTxError "$test_name" "$tx_result"
