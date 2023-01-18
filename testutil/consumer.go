9e7ccfa218440f6b401937d088e0501bc627f106
NicholasDotSol <nicholas.e.evans@gmail.com>
2023-01-18 14:48:55 -0600

    Fix consumer trusting period

--- testutil/consumer.go
+++ testutil/consumer.go
@@ -9,6 +9,7 @@ import (
 	ibctmtypes "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 	ccvconsumertypes "github.com/cosmos/interchain-security/x/ccv/consumer/types"
 	ccvprovidertypes "github.com/cosmos/interchain-security/x/ccv/provider/types"
+	typesccv "github.com/cosmos/interchain-security/x/ccv/types"
 )
 
 // This function creates consumer module genesis state that is used as starting point for modifications
@@ -21,7 +22,7 @@ func CreateMinimalConsumerTestGenesis() *ccvconsumertypes.GenesisState {
 	genesisState.ProviderClientState = ccvprovidertypes.DefaultParams().TemplateClient
 	genesisState.ProviderClientState.ChainId = app.Name
 	genesisState.ProviderClientState.LatestHeight = ibctypes.Height{RevisionNumber: 0, RevisionHeight: 1}
-	genesisState.ProviderClientState.TrustingPeriod = genesisState.Params.UnbondingPeriod
+	genesisState.ProviderClientState.TrustingPeriod, _ = typesccv.CalculateTrustPeriod(genesisState.Params.UnbondingPeriod, ccvprovidertypes.DefaultTrustingPeriodFraction)
 	genesisState.ProviderClientState.UnbondingPeriod = genesisState.Params.UnbondingPeriod
 	genesisState.ProviderClientState.MaxClockDrift = ccvprovidertypes.DefaultMaxClockDrift
 	genesisState.ProviderConsensusState = &ibctmtypes.ConsensusState{
