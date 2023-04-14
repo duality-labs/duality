7ff8dc2e532e022cd2344978ea0a038fdd9b1e3b
Teddy Knox <teddyknox@gmail.com>
2023-04-14 17:28:37 -0400

    Fix lock refs

--- x/incentives/keeper/lock_refs.go
+++ x/incentives/keeper/lock_refs.go
@@ -41,7 +41,7 @@ func getStakeRefKeys(stake *types.Stake) ([][]byte, error) {
 
 	refKeys := [][]byte{}
 	refKeys = append(refKeys, types.KeyPrefixStakeIndex)
-	refKeys = append(refKeys, types.KeyPrefixStakeIndexAccount, owner)
+	refKeys = append(refKeys, types.CombineKeys(types.KeyPrefixStakeIndexAccount, owner))
 
 	for _, coin := range stake.Coins {
 		depositDenom, err := dextypes.NewDepositDenomFromString(coin.Denom)
@@ -51,10 +51,10 @@ func getStakeRefKeys(stake *types.Stake) ([][]byte, error) {
 		denomBz := []byte(coin.Denom)
 		pairIdBz := []byte(depositDenom.PairID.Stringify())
 		tickBz := dextypes.TickIndexToBytes(depositDenom.Tick, depositDenom.PairID, depositDenom.PairID.Token1)
-		refKeys = append(refKeys, types.KeyPrefixStakeIndexDenom, denomBz)
-		refKeys = append(refKeys, types.KeyPrefixStakeIndexPairTick, pairIdBz, tickBz)
-		refKeys = append(refKeys, types.KeyPrefixStakeIndexAccountDenom, owner, denomBz)
-		refKeys = append(refKeys, types.KeyPrefixStakeIndexPairTimestamp, pairIdBz, types.GetTimeKey(stake.StartTime))
+		refKeys = append(refKeys, types.CombineKeys(types.KeyPrefixStakeIndexDenom, denomBz))
+		refKeys = append(refKeys, types.CombineKeys(types.KeyPrefixStakeIndexPairTick, pairIdBz, tickBz))
+		refKeys = append(refKeys, types.CombineKeys(types.KeyPrefixStakeIndexAccountDenom, owner, denomBz))
+		refKeys = append(refKeys, types.CombineKeys(types.KeyPrefixStakeIndexPairTimestamp, pairIdBz, types.GetTimeKey(stake.StartTime)))
 	}
 
 	return refKeys, nil
