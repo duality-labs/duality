695f5a0bd6e7a3c7e32e006f6820f3aea47e9236
Julian Compagni Portis <julian@terrascope.io>
2023-04-14 12:43:12 -0600

    add queries

--- x/incentives/types/errors.go
+++ x/incentives/types/errors.go
@@ -8,7 +8,8 @@ import (
 
 // x/incentives module sentinel errors.
 var (
-	ErrNotLockOwner   = sdkerrors.Register(ModuleName, 1, "msg sender is not the owner of specified lock")
-	ErrLockupNotFound = sdkerrors.Register(ModuleName, 2, "lockup not found")
-	ErrGaugeNotActive = sdkerrors.Register(ModuleName, 3, "cannot distribute from gauges when it is not active")
+	ErrNotLockOwner       = sdkerrors.Register(ModuleName, 1, "msg sender is not the owner of specified lock")
+	ErrLockupNotFound     = sdkerrors.Register(ModuleName, 2, "lockup not found")
+	ErrGaugeNotActive     = sdkerrors.Register(ModuleName, 3, "cannot distribute from gauges when it is not active")
+	ErrInvalidGaugeStatus = sdkerrors.Register(ModuleName, 4, "Gauge status filter must be one of: ACTIVE_UPCOMING, ACTIVE, UPCOMING, FINISHED")
 )
