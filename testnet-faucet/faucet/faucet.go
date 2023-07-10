b2f0dc30e78bf6af1c44d7673646a1684753d9d2
Julian Compagni Portis <julian@terrascope.io>
2023-07-10 13:35:55 -0400

    add ignite cli dep

--- testnet-faucet/faucet/faucet.go
+++ testnet-faucet/faucet/faucet.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
-	"encoding/json"
 	"context"
-	"time"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"log"
+	"net/http"
 	"strings"
-	chaincmdrunner "github.com/tendermint/starport/starport/pkg/chaincmd/runner"
-	"github.com/tendermint/starport/starport/pkg/chaincmd"
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ignite/cli/ignite/pkg/chaincmd"
+	chaincmdrunner "github.com/ignite/cli/ignite/pkg/cmdrunner"
 )
 
 // Default Config vars
-var(
-	Port = "9000"
-	AppCli = "dualityd"
+var (
+	Port          = "9000"
+	AppCli        = "dualityd"
 	FaucetAccount = ""
-	AmountToSend = 1000
-	DenomsToSend = []string{"token", "stake"}
-	TxTimeoutInt = 1
-	TxTimeout = time.Duration(TxTimeoutInt)*time.Second
-	MaxTxRetry = 10
-	NodeAddress = "tcp://localhost:26657"
+	AmountToSend  = 1000
+	DenomsToSend  = []string{"token", "stake"}
+	TxTimeoutInt  = 1
+	TxTimeout     = time.Duration(TxTimeoutInt) * time.Second
+	MaxTxRetry    = 10
+	NodeAddress   = "tcp://localhost:26657"
 )
-
 
 type request struct {
 	Address string
@@ -36,7 +36,7 @@ type response struct {
 	Message string
 }
 
-func newChainCmdRunner()(cr chaincmdrunner.Runner, err error){
+func newChainCmdRunner() (cr chaincmdrunner.Runner, err error) {
 	ccoptions := []chaincmd.Option{
 		// chaincmd.WithKeyringPassword(keyringPassword),
 		chaincmd.WithKeyringBackend(chaincmd.KeyringBackendTest),
@@ -49,48 +49,44 @@ func newChainCmdRunner()(cr chaincmdrunner.Runner, err error){
 	return
 }
 
-func sendToken(cr chaincmdrunner.Runner, address string, amount int, denom string) (error){
-
+func sendToken(cr chaincmdrunner.Runner, address string, amount int, denom string) error {
 	amountStr := fmt.Sprintf("%d%s", amount, denom)
 	log.Printf("Sending: %s", amountStr)
 
 	tx, err := cr.BankSend(context.Background(), FaucetAccount, address, amountStr)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	// TODO: This is very slow because we can only do 1 tx per block. Ideally need to find a way to batch transactions
 	err = cr.WaitTx(context.Background(), tx, TxTimeout, MaxTxRetry)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func sendAllTokens(address string)(error){
+func sendAllTokens(address string) error {
 	cr, err := newChainCmdRunner()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	for _, denom := range DenomsToSend {
 		err := sendToken(cr, address, AmountToSend, denom)
-		if err != nil{
+		if err != nil {
 			return err
 		}
 	}
 
 	return nil
-
 }
 
-func faucetHandler(w http.ResponseWriter, r *http.Request){
-
+func faucetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	address := r.URL.Query().Get("address")
-
 
 	// Ensure that an address is passed in
 	if address == "" {
@@ -101,7 +97,6 @@ func faucetHandler(w http.ResponseWriter, r *http.Request){
 	// Ensure that address is valid
 	// TODO: this is a very awkward way to validate an adresss since we just convert it back to a string
 	accAdress, err := sdk.AccAddressFromBech32(address)
-
 	if err != nil {
 		http.Error(w, "Address is invalid", http.StatusInternalServerError)
 		return
@@ -117,20 +112,16 @@ func faucetHandler(w http.ResponseWriter, r *http.Request){
 	resp := response{Message: fmt.Sprintf("Tokens sent to: %s", accAdress.String())}
 	json.NewEncoder(w).Encode(resp)
 
-
-
 	log.Printf("Sent tokens to: %s", address)
-
 }
 
-func startServer(){
+func startServer() {
 	http.HandleFunc("/", faucetHandler)
 	log.Printf("Listening on http://localhost:%s", Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", Port), nil))
 }
 
 func main() {
-
 	// Handle flags
 	flag.StringVar(&FaucetAccount, "faucet-account", FaucetAccount, "Account to use for faucet")
 	flag.StringVar(&Port, "port", Port, "Port to listen on")
@@ -143,18 +134,17 @@ func main() {
 	DenomsToSend = strings.Split(*denomsToSendStr, ",")
 
 	config := map[string]interface{}{
-		"Port": Port,
-		"AppCli":  AppCli,
-		"FaucetAccount":  FaucetAccount,
-		"AmountToSend": AmountToSend,
-		"DenomsToSend": DenomsToSend,
-		"TxTimeoutInt": TxTimeoutInt,
-		"MaxTxRetry": MaxTxRetry,
-		"Node": NodeAddress,
+		"Port":          Port,
+		"AppCli":        AppCli,
+		"FaucetAccount": FaucetAccount,
+		"AmountToSend":  AmountToSend,
+		"DenomsToSend":  DenomsToSend,
+		"TxTimeoutInt":  TxTimeoutInt,
+		"MaxTxRetry":    MaxTxRetry,
+		"Node":          NodeAddress,
 	}
 	prettyConfig, _ := json.MarshalIndent(config, "", "  ")
 	log.Printf("Starting faucet with config:\n %s", prettyConfig)
 
 	startServer()
-
 }
