e178b9c6c40879407590881a1eb22d518b4fa880
Julian Compagni Portis <jcompagni@gmail.com>
2023-08-08 20:20:56 -0700

    Switch account prefix to "dual" (#454)
    note: some tests that don't initialize the app may need: `var _
    appparams.EncodingConfig` to ensure that the sdk.Config is updated
    before the test runs
    
    ---------
    
    Co-authored-by: Julian Compagni Portis <julian@terrascope.io>

--- tests/e2e/app_initers.go
+++ tests/e2e/app_initers.go
@@ -6,12 +6,17 @@ import (
 	dbm "github.com/cometbft/cometbft-db"
 	"github.com/cometbft/cometbft/libs/log"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	ibctesting "github.com/cosmos/interchain-security/v3/legacy_ibc_testing/testing"
+	icsappiniters "github.com/cosmos/interchain-security/v3/testutil/ibc_testing"
 	"github.com/duality-labs/duality/app"
+	appparams "github.com/duality-labs/duality/app/params"
 )
 
 // DualityAppIniter implements ibctesting.AppIniter for the duality consumer app
 func DualityAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
+	// Reset to duality config before initializing duality app
+	appparams.InitParams()
 	encoding := app.MakeEncodingConfig()
 	testApp := app.NewApp(
 		log.NewNopLogger(),
@@ -27,4 +32,10 @@ func DualityAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	)
 
 	return testApp, app.NewDefaultGenesisState(encoding.Marshaler)
+}
+
+func ProviderAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
+	// Reset to cosmos sdk base config before initializing provider app
+	sdk.GetConfig()
+	return icsappiniters.ProviderAppIniter()
 }
