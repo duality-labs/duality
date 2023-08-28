e4fefc4ca77a54c3736a807da47c7da612d9e50e
Julian Compagni Portis <julian@terrascope.io>
2023-08-28 14:46:52 -0700

    remove dead code

--- x/incentives/types/stakes.go
+++ x/incentives/types/stakes.go
@@ -4,18 +4,6 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 type Stakes []*Stake
 
-// JCP TODO: remove?
-// func (stakes Stakes) CoinsByQueryCondition(distrTo QueryCondition) sdk.Coins {
-// 	coins := sdk.Coins{}
-// 	for _, stake := range stakes {
-// 		coinsToAdd := stake.CoinsPassingQueryCondition(distrTo)
-// 		if !coinsToAdd.Empty() {
-// 			coins = coins.Add(coinsToAdd...)
-// 		}
-// 	}
-// 	return coins
-// }
-
 func (stakes Stakes) GetCoins() sdk.Coins {
 	coins := sdk.Coins{}
 	for _, stake := range stakes {
