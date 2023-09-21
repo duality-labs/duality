51dd9a4b7560601538cdd3470e55e0929e006835
Julian Compagni Portis <julian@terrascope.io>
2023-09-21 16:59:23 -0600

    fix rebase

--- x/dex/types/tick_liquidity_key.go
+++ x/dex/types/tick_liquidity_key.go
@@ -1,6 +1,8 @@
 package types
 
+import math_utils "github.com/duality-labs/duality/utils/math"
+
 type TickLiquidityKey interface {
 	KeyMarshal() []byte
-	PriceTakerToMaker() (priceTakerToMaker sdk.Dec, err error)
+	PriceTakerToMaker() (priceTakerToMaker math_utils.PrecDec, err error)
 }
